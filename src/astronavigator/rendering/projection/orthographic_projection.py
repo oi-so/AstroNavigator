af8c3d58e3dad74338c4eec76232bbc232fd2008
oi-so <mail@oioi-2020.com>
2026-07-18 12:11:52 +0900

    Projectionクラスを修正

--- src/astronavigator/rendering/projection/orthographic_projection.py
+++ src/astronavigator/rendering/projection/orthographic_projection.py
@@ -1,7 +1,25 @@
 from __future__ import annotations
 
+from PySide6.QtCore import QPointF, QSize
+
+from astronavigator.camera.sky_camera import SkyCamera
 from astronavigator.rendering.projection.projection import Projection
+from astronavigator.sky.position import Position
 
 
 class OrthographicProjection(Projection):
-    ...
+    def project(
+        self, 
+        position: Position, 
+        camera: SkyCamera, 
+        viewport_size: QSize
+    ) -> QPointF | None:
+        raise NotImplementedError("Orthographic projection is not implemented yet.")
+    
+    def unproject(
+        self, 
+        screen_position: QPointF, 
+        camera: SkyCamera, 
+        viewport_size: QSize
+    ) -> Position:
+        raise NotImplementedError("Orthographic unprojection is not implemented yet.")
