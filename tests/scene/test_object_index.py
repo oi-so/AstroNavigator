8d50d4eecf42ec7c3d390d2e4b0cc865f31a5592
oi-so <mail@oioi-2020.com>
2026-07-22 00:11:01 +0900

    ObjectIndexをSceneControllerから使えるように実装

--- tests/scene/test_object_index.py
+++ tests/scene/test_object_index.py
@@ -1,8 +1,11 @@
+from astronavigator.event.event_bus import EventBus
 from astronavigator.scene.object_index import ObjectIndex
+from astronavigator.scene.scene_controller import SceneController
 from astronavigator.sky.magnitude import Magnitude
 from astronavigator.sky.object_type import ObjectType
 from astronavigator.sky.position import Position
 from astronavigator.sky.sky_object import Star
+from astronavigator.scene.scene import Scene
 
 
 def create_star(
@@ -101,3 +104,32 @@ def test_update_replaces_objects():
 
     assert index.find_by_id("star1") is None
     assert index.find_by_id("star2") is star2
+
+
+def test_add_object_updates_object_index():
+    star1 = create_star("star1", "Sirius")
+    star2 = create_star("star2", "Betelgeuse")
+
+    scene = Scene()
+    scene_controller = SceneController(scene, event_bus=EventBus())
+
+    scene_controller.add_object(star1)
+    scene_controller.add_object(star2)
+
+    assert scene.object_index.find_by_id("star1") is star1
+    assert scene.object_index.find_by_id("star2") is star2
+
+def test_remove_object_updates_object_index():
+    star1 = create_star("star1", "Sirius")
+    star2 = create_star("star2", "Betelgeuse")
+
+    scene = Scene()
+    scene_controller = SceneController(scene, event_bus=EventBus())
+
+    scene_controller.add_object(star1)
+    scene_controller.add_object(star2)
+
+    scene_controller.remove_object(star1)
+
+    assert scene.object_index.find_by_id("star1") is None
+    assert scene.object_index.find_by_id("star2") is star2
