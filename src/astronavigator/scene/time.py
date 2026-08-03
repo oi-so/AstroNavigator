29150bf354cc9ffbe185a52878d12008380dbb9e
oi-so <mail@oioi-2020.com>
2026-08-03 15:52:27 +0900

    TimePanelを作成し配置

--- src/astronavigator/scene/time.py
+++ src/astronavigator/scene/time.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime, timezone
-
+from zoneinfo import ZoneInfo
 
 
 
@@ -13,3 +13,20 @@ class Time:
     @classmethod
     def now(cls) -> "Time":
         return cls(utc=datetime.now(timezone.utc))
+
+
+    def to_local_time(self, timezone: ZoneInfo) -> datetime:
+        return self.utc.astimezone(timezone)
+
+
+    def get_date_string(self, zone_info: ZoneInfo) -> str:
+        local_time = self.to_local_time(zone_info)
+        return local_time.strftime("%Y-%m-%d")
+
+    def get_time_string(self, zone_info: ZoneInfo) -> str:
+        local_time = self.to_local_time(zone_info)
+        return local_time.strftime("%H:%M:%S")
+
+    def get_datetime_string(self, zone_info: ZoneInfo) -> str:
+        local_time = self.to_local_time(zone_info)
+        return local_time.strftime("%Y-%m-%d %H:%M:%S %Z")
