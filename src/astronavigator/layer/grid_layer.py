669252f3a141c6d4fb4c31f328ac63759956e584
oi-so <mail@oioi-2020.com>
2026-08-17 01:53:48 +0900

    グリッドのパスのどちらか一方の点が描画範囲外だと、線が描画されない問題を修正

--- src/astronavigator/layer/grid_layer.py
+++ src/astronavigator/layer/grid_layer.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import TypeVar
-from PySide6.QtCore import QPointF
+from PySide6.QtCore import QPointF, Qt
 from PySide6.QtGui import QColor
 
 from astronavigator.layer.layer import Layer, LayerType
@@ -46,26 +46,42 @@ class GridLayer(Layer):
         self._labels.clear()
         grid_settings = context.scene.rendering_settings.grid_settings
 
-        for grid in self.grid_manager.grids():
-            if not context.scene.rendering_settings.grid_settings.is_visible.get(grid.coordinate_system, False):
-                continue
+        painter = context.painter
+        painter.save()
 
-            color = context.scene.rendering_settings.grid_settings.colors.get(grid.coordinate_system)
-            context.painter.setPen(color)
+        try:
+            clip_path = context.projection.create_clip_path(
+                context.projection_context,
+                context.viewport.size()
+            )
+            painter.setClipPath(clip_path, Qt.ClipOperation.IntersectClip)
 
-            for line in grid.iter_lines(context):
-                anchor = self._draw_line(context, grid.coordinate_system, line.positions)
-
-                if anchor is None:
+            for grid in self.grid_manager.grids():
+                if not grid_settings.is_visible.get(grid.coordinate_system, False):
                     continue
 
-                label_color = QColor(color)
-                label_color.setAlpha(max(label_color.alpha(), GRID_LABEL_MINIMUM_ALPHA))
-                self._labels.append(GridLabel(text=line.label, color=label_color, anchor=anchor))
+                color = grid_settings.colors.get(grid.coordinate_system)
+                if color is None:
+                    continue
+
+                painter.setPen(color)
+
+                for line in grid.iter_lines(context):
+                    anchor = self._draw_line(context, grid.coordinate_system, line.positions)
+                    if anchor is None:
+                        continue
+
+                    label_color = QColor(color)
+                    label_color.setAlpha(max(label_color.alpha(), GRID_LABEL_MINIMUM_ALPHA))
+                    self._labels.append(GridLabel(text=line.label, color=label_color, anchor=anchor))
+        finally:
+            painter.restore()
 
     # @profile
     def _draw_line(self, context: RendererContext, coordinate_system: CoordinateSystem, positions: Iterable[T]) -> QPointF | None:
-        previous: QPointF | None = None
+        previous_line_point: QPointF | None = None
+        previous_is_visible = False
+
         closest_point: QPointF | None = None
         closest_edge_distance = float("inf")
 
@@ -74,25 +90,37 @@ class GridLayer(Layer):
         height = float(viewport_size.height())
 
         for position in positions:
-            point = context.projection.project_grid_position(
-                position,
-                coordinate_system,
-                context.projection_context,
-                context.viewport.size()
+            visible_point = context.projection.project_grid_position(
+                position, coordinate_system, context.projection_context, viewport_size
             )
 
-            if point is None:
-                previous = None
+            line_point = context.projection.project_grid_position_unclipped(
+                position, coordinate_system, context.projection_context, viewport_size
+            )
+
+            if line_point is None:
+                previous_line_point = None
+                previous_is_visible = False
                 continue
 
-            if previous is not None:
-                context.painter.drawLine(previous, point)
+            is_visible = visible_point is not None
 
-            edge_distance = min(point.x(), width - point.x(), point.y(), height - point.y())
-            if edge_distance < closest_edge_distance:
-                closest_edge_distance = edge_distance
-                closest_point = point
+            if previous_line_point is not None and (previous_is_visible or is_visible):
+                context.painter.drawLine(previous_line_point, line_point)
 
-            previous = point
+            if visible_point is not None:
+                edge_distance = min(
+                    visible_point.x(), 
+                    width - visible_point.x(), 
+                    visible_point.y(), 
+                    height - visible_point.y()
+                    )
+                
+                if edge_distance < closest_edge_distance:
+                    closest_edge_distance = edge_distance
+                    closest_point = visible_point
+
+            previous_line_point = line_point
+            previous_is_visible = is_visible
 
         return closest_point
